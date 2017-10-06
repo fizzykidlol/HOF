86ae8efcce212102f3de32bd7ffe799a090e56f1
Ross <cc4455666@qq.com>
2017-10-06 17:20:09 +1000

    First level room unlock door with key added
    Move the key box to the corner of room

---
+++ Assets/Scripts/DoorKey.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorKey : MonoBehaviour {
+
+    public bool inTrigger;
+
+    void OnTriggerEnter(Collider other) {
+        inTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other) {
+        inTrigger = false;
+    }
+
+    void Update() {
+        if (inTrigger) {
+            if (Input.GetKeyDown(KeyCode.E)) {
+                DoorScript.doorKey = true;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    void OnGUI() {
+        if (inTrigger) {
+            GUI.Box(new Rect(800, 400, 200, 25), "Press E to take key");
+        }
+    }
+}
